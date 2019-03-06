e732b0c89b9db34751290014bd8624f71a79702a
Andrey <podshik93@ukr.net>
2019-03-06 16:04:47 +0200

    New FILE

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Test</title>
+</head>
+<body>
+<ul>
+    <li>Hello World </li>
+</ul>
+<h1>Hello World!!!!!</h1>
+<p>Dont worry </p>
+</body>
+</html>
